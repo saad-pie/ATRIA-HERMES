e4a7108c4c110176e3408416dc04f4059ee6b011
saad-pie <saadabdulrehman7474@gmail.com>
2026-09-18 14:27:58 +0500

    Initialize React application with main entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import HermesAgentUI from './App.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HermesAgentUI />
+  </React.StrictMode>,
+)
